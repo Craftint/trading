64bd97dcadf49a4d49dd3d51a694ce2cb003cca6
kunhimohamed <kunhimohamed6@gmail.com>
2021-08-14 20:29:56 +0400

    Fix: manual autoname set at the first name, no change after save, for now fix later

--- trading/events/delivery_note.py
+++ trading/events/delivery_note.py
@@ -2,5 +2,10 @@ from __future__ import unicode_literals
 from frappe import _
 
 def change_autoname(doc, handler=None):
-	if doc.manual_naming_series:
+	"""
+	maped the manual autoname in delivery note
+	"""
+	if doc.manual_naming_series and doc.is_new():
 		doc.name = doc.manual_naming_series
+		for item in doc.items:
+			item.parent = doc.manual_naming_series
--- trading/events/sales_invoice.py
+++ trading/events/sales_invoice.py
@@ -1,10 +1,16 @@
 from __future__ import unicode_literals
 from frappe import _
+import frappe
 from frappe.utils import formatdate
 
 def change_autoname_and_remarks(doc, handler=None):
-	if doc.manual_naming_series:
+	"""
+	check and change autoname remarks
+	"""
+	if doc.manual_naming_series and doc.is_new():
 		doc.name = doc.manual_naming_series
+		for item in doc.items:
+			item.parent = doc.manual_naming_series
 	if not doc.remarks:
 			if doc.po_no and doc.po_date:
 				doc.remarks = _("Against Customer Order {0} dated {1}").format(doc.po_no,
